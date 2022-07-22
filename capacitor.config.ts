ca3957eaa9af3b75522cf1d3bca7ead6da9fa4c7
Saaad007 <saad@bbits.solutions>
2022-07-22 18:32:18 +0500

    changed app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,23 +5,6 @@ const config: CapacitorConfig = {
   appName: 'Connected Parking',
   webDir: 'build',
   bundledWebRuntime: false,
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 3000,
-      launchAutoHide: true,
-      backgroundColor: "#ffffffff",
-      androidSplashResourceName: "splash",
-      androidScaleType: "CENTER_CROP",
-      showSpinner: true,
-      androidSpinnerStyle: "large",
-      iosSpinnerStyle: "small",
-      spinnerColor: "#999999",
-      splashFullScreen: true,
-      splashImmersive: true,
-      layoutName: "launch_screen",
-      useDialog: true,
-    },
-  },
 };
 
 export default config;
